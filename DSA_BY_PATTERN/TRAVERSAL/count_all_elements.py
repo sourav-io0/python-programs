14d1d51590e8cc43afa6a0f1ae446f6c72a97916
sourav-io0 <souravnextgen00@gmail.com>
2026-08-24 22:18:04 +0530

    Pattern Wise DSA

---
+++ DSA_BY_PATTERN/TRAVERSAL/count_all_elements.py
@@ -0,0 +1,8 @@
+arr=[10,20,30,40,50]
+count=0
+for i in range (len(arr)):
+    count+=1
+
+    
+print(count)
+    
---
+++ DSA_BY_PATTERN/TRAVERSAL/sum_of_all_elements.py
@@ -0,0 +1,6 @@
+arr=[10,20,30,40,50]
+total=0
+for i in range (len(arr)):
+
+    total+=arr[i]
+print(total)    
